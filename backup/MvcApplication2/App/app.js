35ace92803461d79a838279a61f51d44078e4ceb
lelong37 <lelong37@gmail.com>
2014-05-19 00:30:43 -0500

    backing up burke's project

---
+++ backup/MvcApplication2/App/app.js
@@ -0,0 +1,25 @@
+﻿define([
+    'angular-kendo'
+], function (layoutController, homeController) {
+
+    //var start = function (app) {
+
+    //    app.config(function ($routeProvider) {
+    //        $routeProvider
+    //            .when('/', {
+    //                templateUrl: 'partials/home.html',
+    //                controller: 'HomeController'
+    //            })
+    //            .when('/details', {
+    //                templateUrl: 'partials/details.html',
+    //                controller: 'DetailsController'
+    //            });
+    //    });
+
+    //    angular.bootstrap(document, [ 'App' ]);
+    //}
+    
+
+    return angular.module('app', ['kendo.directives']);
+
+});
