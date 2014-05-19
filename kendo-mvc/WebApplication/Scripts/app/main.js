37235e696570471179cfeba98fea21ae3ee4af20
lelong37 <lelong37@gmail.com>
2014-05-19 02:17:41 -0500

    requirejs main file

---
+++ kendo-mvc/WebApplication/Scripts/app/main.js
@@ -0,0 +1,29 @@
+﻿
+require.config({
+    paths: {
+        //packages
+        'jquery': '/scripts/jquery-2.1.1.min',
+        'kendo': '/scripts/kendo/2014.1.318/kendo.web.min',
+        'text': '/scripts/text',
+        'router': '/scripts/app/router',
+        //models
+        //viewModels
+        'home/indexViewModel': 'viewModels/home/indexViewModel',
+        'home/contactViewModel': 'viewModels/home/contactViewModel',
+        'home/aboutViewModel': 'viewModels/home/aboutViewModel',
+        //datasources
+        // utils
+        'util': '/scripts/util'
+    },
+    shim: {
+        'kendo': ['jquery']
+    },
+    priority: ['text', 'router', 'app'],
+    jquery: '2.1.1'
+});
+
+require([
+        'app'
+], function (app) {
+    app.initialize();
+});
