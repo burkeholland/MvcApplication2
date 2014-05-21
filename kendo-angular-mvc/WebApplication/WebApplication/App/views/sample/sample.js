46a9b285cb67277613c15388ccf3ee0acb74c377
lelong37 <lelong37@gmail.com>
2014-05-21 04:04:53 -0500

    baseline (big) check-in angularjs / kendo

---
+++ kendo-angular-mvc/WebApplication/WebApplication/App/views/sample/sample.js
@@ -0,0 +1,7 @@
+﻿'use strict';
+
+webApplication.controller('sampleController',
+    function ($scope, productDataSource) {
+        $scope.title = 'Sample';
+        $scope.productDataSource = productDataSource;
+    });
